4a98ce4a6ebdc898113e0126743d6cb57e485192
vic1er03 <azariaazaria473@gmail.com>
2025-04-07 18:26:31 +0100

    Update dashboard_final.py

--- dashboard_final.py
+++ dashboard_final.py
@@ -364,7 +364,12 @@ def analyze_categorical_relationships(df, sheet_name):
                     fig = px.bar(contingency_pct, 
                                  barmode='stack',
                                  title=f"{target_column} vs {col} (p={p:.4f})",
-                                 labels={'value': 'Pourcentage (%)', 'index': target_column})
+                                 labels={'value': 'Pourcentage (%)', 'index': target_column},
+                                text=contingency_pct['value'])
+                    
+                    # Afficher automatiquement les textes sur les barres
+                    fig.update_traces(texttemplate='%{text:.1f}%', textposition='inside')
+                    
                     fig.update_layout(template='plotly_white')
                     st.plotly_chart(fig)
 
