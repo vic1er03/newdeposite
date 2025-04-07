4e5a41aa01814b27629507380925b95b0166180f
vic1er03 <azariaazaria473@gmail.com>
2025-04-07 16:56:11 +0100

    Update dashboard_final.py

--- dashboard_final.py
+++ dashboard_final.py
@@ -1032,22 +1032,15 @@ def main():
     
     
     if page == "Accueil":
-        # Créer deux colonnes pour placer l’image en haut
-        col1, col2 = st.columns([2, 3])  # Ajuste les ratios selon le design souhaité
-        image_file="Image_sang.jpg"
-        with col1:
-            image = Image.open(image_file)
-            st.image(image, use_column_width=True)
-        
-        with col2:
-            st.write("")  # Espace vide ou texte complémentaire si tu veux
-        
-            st.markdown('<p style="color:black;">Texte en noir</p>', unsafe_allow_html=True)
-            st.title("📊 Tableau de Bord d'Analyse des Donneurs de Sang")
-            st.markdown("""
-            Ce tableau de bord interactif présente une analyse approfondie des données de donneurs de sang,
-            permettant d'optimiser les campagnes de don et d'améliorer la gestion des donneurs.
-            """)
+        # Afficher l'image en haut, sur toute la largeur
+    image = Image.open(image_file)
+    st.image(image, use_container_width=True)  # ✅ remplace use_column_width     
+    st.markdown('<p style="color:black;">Texte en noir</p>', unsafe_allow_html=True)
+    st.title("📊 Tableau de Bord d'Analyse des Donneurs de Sang")
+    st.markdown("""
+    Ce tableau de bord interactif présente une analyse approfondie des données de donneurs de sang,
+    permettant d'optimiser les campagnes de don et d'améliorer la gestion des donneurs.
+    """)
         
         #set_background(image_file)
         """
