15dd2733a169b8fc18c004a1fa0868b9341d2a7b
vic1er03 <azariaazaria473@gmail.com>
2025-04-08 20:38:59 +0100

    Update dashboard_final.py

--- dashboard_final.py
+++ dashboard_final.py
@@ -1915,8 +1915,6 @@ def main():
             
             5. **Fidélisation**: Mettez en place des stratégies pour encourager les donneurs à revenir régulièrement.
             """)
-        else :
-            st.warning("Impossible d'analyser les tendances temporelles car aucune colonne de date appropriée n'a été identifiée.")
     
     elif page == "Fidélisation des donneurs":
         # Afficher l'image en haut, sur toute la largeur
